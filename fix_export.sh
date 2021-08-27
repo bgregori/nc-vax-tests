2eff33f7d381c6066efc77baaac3b358324f9805
Bryan Gregorius <bgregori@redhat.com>
2021-08-27 14:57:53 -0400

    Add fix_export helper script.

---
+++ fix_export.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+######################################################################### 
+# 
+#   Takes an exported SeleniumIDE Mocha test and substitutes our hooks
+#   for pulling in values via the global.js module.
+#
+#   USAGE: ./fix_export <filename>
+######################################################################### 
+
+sed -i '' 's/usingServer(.*)\./usingServer(globals.webDriverUrl)\./g' $1
+sed -i '' 's/driver.get(.*)/driver.get(globals.appRoute)/g' $1
+sed -i '' "s/\'chrome\'/globals.browser/g" $1
+sed -i '' "1s;^;const globals = require\(\'../globals.js\'\)\n\n;" $1
+sed -i '' "s/beforeEach/before/g" $1
+sed -i '' "s/afterEach/after/g" $1
+
