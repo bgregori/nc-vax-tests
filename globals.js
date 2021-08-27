be15034e7df3b7050476e6e7280f667ac02eb1bd
Bryan Gregorius <bgregori@redhat.com>
2021-08-27 14:58:23 -0400

    Add support for BROWSER env var

--- globals.js
+++ globals.js
@@ -1,7 +1,5 @@
-//const appRoute = 'http://simple-ui2-simpleui.apps.valkyrie.rhnaps.io';
-//const webDriverUrl = 'http://se-hub-devsecops.apps.valkyrie.rhnaps.io/wd/hub';
-
 module.exports =  {
     appRoute: process.env.APP_ROUTE,
-    webDriverUrl: process.env.WEB_DRIVER_URL
+    webDriverUrl: process.env.WEB_DRIVER_URL,
+    browser: process.env.BROWSER || "chrome",  
 };
